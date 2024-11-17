15ab6589c440e7d0cf1902f916f8f91447aa8326
Phinix3d <saraahaik@gmail.com>
2024-11-17 09:40:42 +0100

    updatet free function + testat

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cahaik <cahaik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 12:25:58 by cahaik            #+#    #+#             */
-/*   Updated: 2024/11/17 09:39:13 by cahaik           ###   ########.fr       */
+/*   Updated: 2024/11/17 09:40:22 by cahaik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,51 +46,51 @@ int main(int ac, char **av, char **env)
 	(void)ac;
 	(void)av;
 	ev = environ(env);
-	// signals(1);
-	// str = readline("minii>");
-	// trim = ft_strtrim(str, " \t");
-	// free(str);
-	// str = trim;
-	// p.check_redir = 0;
-	// p.for_redir_check = 0;
-	// p.exit_status = 0;
-	// while (str)
-	// {
-	// 	signals(1);
-	// 	if (gqq_signal == 1)
-	// 	{
-	// 		p.exit_status = 1;
-	// 		gqq_signal = 0;
-	// 	}
-	// 	data.cmnd = str;
-	// 	data.tokens_list = NULL;
-	// 	add_history(str);
-	// 	lexer(&data);
-	// 	token_node = data.tokens_list;
-	// 	// printf("3ela berra \n");
-	// 	if(!syntaxe_validation(token_node))
-	// 	{
-	// 		// printf("dakheel 3lyha \n");
-	// 		expand_env(data.tokens_list, ev, &p);
-	// 		remove_quotes((data.tokens_list));
-	// 		tree = build_ast((data.tokens_list), ev, &p);
-	// 		// if (!tree->cmnd && tree->args && tree->args[0])
-	// 		// 	tree->cmnd = ft_strdup(tree->args[0]);
-	// 		current_tree = tree;
-	// 		tree->ev = ev;
-	// 		unlink_herdoc = *tree;
-	// 		execution(tree, &p);
-	// 		last_herdoc_number(unlink_herdoc, 1);
-	// 		// print_ast(tree, 0);
-	// 	}
+	signals(1);
+	str = readline("minii>");
+	trim = ft_strtrim(str, " \t");
+	free(str);
+	str = trim;
+	p.check_redir = 0;
+	p.for_redir_check = 0;
+	p.exit_status = 0;
+	while (str)
+	{
+		signals(1);
+		if (gqq_signal == 1)
+		{
+			p.exit_status = 1;
+			gqq_signal = 0;
+		}
+		data.cmnd = str;
+		data.tokens_list = NULL;
+		add_history(str);
+		lexer(&data);
+		token_node = data.tokens_list;
+		// printf("3ela berra \n");
+		if(!syntaxe_validation(token_node))
+		{
+			// printf("dakheel 3lyha \n");
+			expand_env(data.tokens_list, ev, &p);
+			remove_quotes((data.tokens_list));
+			tree = build_ast((data.tokens_list), ev, &p);
+			// if (!tree->cmnd && tree->args && tree->args[0])
+			// 	tree->cmnd = ft_strdup(tree->args[0]);
+			current_tree = tree;
+			tree->ev = ev;
+			unlink_herdoc = *tree;
+			execution(tree, &p);
+			last_herdoc_number(unlink_herdoc, 1);
+			// print_ast(tree, 0);
+		}
 		
-	// 	free(str);  
-	// 	// printf("ltee7t \n");
-	// 	signals(1);
-	// 	str = readline("minii>");
-	// 	trim = ft_strtrim(str, " \t");
-	// 	free(str);
-	// 	str = trim;
-	// }
+		free(str);  
+		// printf("ltee7t \n");
+		signals(1);
+		str = readline("minii>");
+		trim = ft_strtrim(str, " \t");
+		free(str);
+		str = trim;
+	}
 	return (p.exit_status); // update
 }
